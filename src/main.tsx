e7518801ab5789ad7ef04e7b7c383fba4247e64c
Mohamad-Gh <ghalebizadem@gmail.com>
2025-08-07 07:39:53 -0700

    add lazyloading and remove app.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App";
-import ProjectsPage from "./pages/ProjectsPage";
-import "./index.css";
-import NotFound from "./pages/NotFound";
-import Layout from "./pages/Layout";
+import { lazy, Suspense } from "react";
 
-// ReactDOM.createRoot(document.getElementById("root")!).render(
-//   <React.StrictMode>
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<App />} />
-//         <Route path="/projects" element={<ProjectsPage />} />
-//         <Route path="*" element={<NotFound />} />
-//       </Routes>
-//     </Router>
-//   </React.StrictMode>
-// );
+import "./index.css";
+
+const Layout = lazy(() => import("./pages/Layout"));
+const Home = lazy(() => import("./pages/Home"));
+const ProjectsPage = lazy(() => import("./pages/ProjectsPage"));
+// const ProjectDetails = lazy(() => import("./pages/ProjectDetails"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
-      { index: true, element: <App /> },
+      { index: true, element: <Home /> },
       { path: "projects", element: <ProjectsPage /> },
+      { path: "*", element: <NotFound /> },
     ],
   },
 ]);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Suspense
+      fallback={<div className="p-8 text-center">Loading...</div>}
+    ></Suspense>
     <RouterProvider router={router} />
+    <Suspense />
   </React.StrictMode>
 );
