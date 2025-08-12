ffb5ec4f9be5ce1d5724f97bc6ebbf7e23dbe74d
Mohamad-Gh <ghalebizadem@gmail.com>
2025-08-12 10:02:31 -0700

    adding images for my projects and creating a project database

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -69,8 +69,8 @@ export default function Contact() {
   };
 
   return (
-    <section id="contact" className="py-6 space-y-6">
-      <h2 className="text-2xl font-semibold text-center">Contact Me</h2>
+    <section id="contact" className="py-10 space-y-6">
+      <h2 className="text-2xl font-semibold text-center py-6">Contact Me</h2>
       <div className="flex gap-6 sm:flex-col sm:items-center lg:flex-row lg:items-stretch">
         <form
           onSubmit={handleSubmit(onSubmit)}
