7385d8eee57956601eb05ca3dc8779b3e95006af
Mohamad-Gh <ghalebizadem@gmail.com>
2025-08-12 11:22:58 -0700

    adding more images

--- src/data/ProjectsData.ts
+++ src/data/ProjectsData.ts
@@ -1,6 +1,10 @@
 import { type IconType } from "react-icons";
 
 // Images
+import OMGDigital_FrontPage from "@/assets/images/projects/OMGDigitals/OMGDigital_FrontPage.jpg";
+import TrendMarket_FrontPage from "@/assets/images/projects/Trend_Market/Trend_Market_FrontPage.jpg";
+import FoodHut_FrontPage from "@/assets/images/projects/Food_Hut_App/Food_Hut_Front_Page.jpg";
+// ICONS
 import { FaReact, FaNodeJs, FaGithub } from "react-icons/fa";
 import { SiTailwindcss, SiTypescript, SiGraphql } from "react-icons/si";
 
@@ -38,45 +42,61 @@ const projects: Project[] = [
     liveUrl: "https://fullstack-blog-live.com",
   },
   {
-    id: "2",
-    title: "E-commerce Store",
-    description:
-      "A modern e-commerce platform powered by Next.js, Stripe, Firebase, and Tailwind CSS.",
+    id: "4",
+    title: "Food Hut App",
+    description: "",
     technologiesWithIcons: [
       { name: "TypeScript", icon: SiTypescript },
       { name: "Tailwind CSS", icon: SiTailwindcss },
       { name: "Github", icon: FaGithub },
+      { name: "aos" },
+      { name: "material UI" },
+      { name: "react-icons" },
+      { name: "react-slick" },
+      { name: "slick-carousel" },
+      { name: "tailwindcss" },
+      { name: "vite" },
     ],
     thumbnail: "/images/ecommerce-store.png",
     gallery: ["/images/ecommerce-store.png"],
-    repoUrl: "https://github.com/yourusername/ecommerce-store",
-    liveUrl: "https://ecommerce-store-live.com",
+    repoUrl:
+      "https://github.com/Mohamad-Gh/React_Practices/tree/main/my-food-app",
+    liveUrl: "https://react-food-app-rouge.vercel.app/",
   },
   {
-    id: "3",
+    id: "5",
     title: "Trend Market App",
     description:
-      "A sleek portfolio application built with React, Framer Motion, shadcn/ui, and TypeScript.",
+      "write a description for this website that you can trak your favourite stock add it to your list and also check the history digram of it's changes",
     technologiesWithIcons: [
       { name: "React", icon: FaReact },
-      { name: "TypeScript", icon: SiTypescript },
+      { name: "Vite" },
+      { name: "react-apexcharts" },
+      { name: "react-icons" },
+      { name: "react-router-dom" },
+      { name: "axios" },
+      { name: "apexcharts" },
     ],
-    thumbnail: "/images/portfolio.png",
-    gallery: ["/images/portfolio.png"],
-    repoUrl: "https://github.com/yourusername/portfolio",
-    liveUrl: "https://portfolio-live.com",
+    thumbnail: TrendMarket_FrontPage,
+    gallery: [TrendMarket_FrontPage],
+    repoUrl:
+      "https://github.com/Mohamad-Gh/React_Practices/tree/main/react-trend/react-trend-app",
+    liveUrl: "https://react-practices-wine.vercel.app/",
   },
   {
-    id: "4",
+    id: "6",
     title: "OMC Digital FrontEnd App",
     description:
       "A comprehensive platform designed for startups to accelerate growth, connect with clients, manage employees, promote their brand, and enhance market presence through intuitive tools and services.",
     technologiesWithIcons: [
       { name: "React", icon: FaReact },
-      { name: "TypeScript", icon: SiTypescript },
+      { name: "CSS" },
+      { name: "HTML" },
+      { name: "jQuery" },
+      { name: "Github", icon: FaGithub },
     ],
-    thumbnail: "/OMGDigital_FrontPage.jpg",
-    gallery: ["/OMGDigital_FrontPage.jpg"],
+    thumbnail: OMGDigital_FrontPage,
+    gallery: [OMGDigital_FrontPage],
     repoUrl:
       "https://github.com/Mohamad-Gh/portfolio/tree/main/projects/OMGDigital",
     liveUrl:
